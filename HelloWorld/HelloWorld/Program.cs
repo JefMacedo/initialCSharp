7958e83c6a101e546389ce5e6d7456aa7b5c5a85
Jeferson Macedo <39282384+JefMacedo@users.noreply.github.com>
2022-10-05 09:17:47 -0400

    Implementando um switch para verificar quantos dias tem o mês digitado

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -74,6 +74,9 @@ string frase3 = $"{frase} Ensolarado!";
 
 Console.WriteLine(frase3); */
 
+/*
+ //// Condicional IF
+
 const string aprovado = "Aprovado!";
 const string reprovado = "Reprovado!";
 const string emRecuperacao = "Em Recuperação!";
@@ -90,4 +93,41 @@ if (nota >= 5)
 } else
 {
     Console.WriteLine(reprovado);
+}*/
+
+// Condicional Switch
+const string DIAS_31 = "Este mês tem 31 dias!";
+const string DIAS_30 = "Este mês tem 30 dias!";
+const string DIAS_28 = "Este mês tem 28 dias!";
+const string MES_INVALIDO = "Este mês não existe!";
+
+Console.WriteLine("Escreva um mês: ");
+string mes = Console.ReadLine();
+
+switch (mes.ToUpper())
+{
+    case "JANEIRO":
+    case "MARÇO":
+    case "MAIO":
+    case "JULHO":
+    case "AGOSTO":
+    case "OUTUBRO":
+    case "DEZEMBRO":
+        Console.WriteLine(DIAS_31);
+        break;
+
+    case "ABRIL":
+    case "JUNHO":
+    case "SETEMBRO":
+    case "NOVEMBRO":
+        Console.WriteLine(DIAS_30);
+        break;
+
+    case "FEVEREIRO":
+        Console.WriteLine(DIAS_28);
+        break;
+
+    default:
+        Console.WriteLine(MES_INVALIDO);
+        break;
 }
