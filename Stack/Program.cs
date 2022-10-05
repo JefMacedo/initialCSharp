adaf35c94b8519e00561bf18004ddf6141291dac
Jeferson Macedo <39282384+JefMacedo@users.noreply.github.com>
2022-10-05 11:09:29 -0400

    Last in, last out

---
+++ Stack/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ Stack segue o conceito de LIFO - Last In, First Out
+ */
+
+Stack<string> lista = new Stack<string> ();
+
+lista.Push("Olá");
+lista.Push("Pessoal");
+lista.Push("Do curso");
+
+foreach(string palavra in lista)
+    Console.WriteLine(palavra);
+
+Console.WriteLine("--------------/--------------");
+
+Console.WriteLine(lista.Pop());
+
+Console.WriteLine("--------------/--------------");
+
+foreach (string palavra in lista)
+    Console.WriteLine(palavra);
+
+Console.WriteLine("--------------/--------------");
+
+Console.WriteLine(lista.Peek());
+
